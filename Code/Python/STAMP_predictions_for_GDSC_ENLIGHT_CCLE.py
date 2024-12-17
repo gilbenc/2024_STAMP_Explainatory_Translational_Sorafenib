4e8f44c3006ca491ceca1c6ef54171d35105197c
gilbenc <gil.bencohen@mail.huji.ac.il>
2024-12-17 15:15:49 +0200

    Sorafenib project related models are now available.

--- Code/Python/STAMP_predictions_for_GDSC_ENLIGHT_CCLE.py
+++ Code/Python/STAMP_predictions_for_GDSC_ENLIGHT_CCLE.py
@@ -6,19 +6,27 @@ from data.datasets import TCGADataset
 from matplotlib import pyplot as plt
 import scipy
 import math
+import pickle
+
+PATH = "/home/shair/Desktop/Gil/2024_STAMP_Explainatory_Translational_Sorafenib/"
 
 
 ### Functions
 
-# returns model of model_type, using data_type, for cancer_type & gene combination
-def load_model(data_type, model_type, cancer_type, gene):
-    PATH_model = "/home/gil/Desktop/GCN/GCN_analysis/2.models/" + data_type + "/" + model_type + "/"
+# returns model of model_type, for cancer_type & gene combination
+def load_model(model_type, cancer_type, gene):
+    PATH_model = PATH + "STAMP_models/Model_files/" + model_type + "/"
     pattern = "*"+'_'+cancer_type + "_" + gene+"*"
     model_path = find(pattern, PATH_model)
     if len(model_path) > 1:
         print("error, too many files fit description.")
         exit()
-    return torch.load(model_path[0])
+    if model_type == "GCN":
+        return torch.load(model_path[0])
+    else:
+        with open(model_path[0], 'rb') as file:
+            Classical_model = pickle.load(file)
+        return Classical_model
 
 # subfunction for load_model
 # return 'result' containing file name (should be only one) in 'path' with 'pattern' in them
@@ -38,11 +46,11 @@ def find(pattern, path):
 # e. compute zscore(log(rsem values))
 # f. add first degree missing columns as 0 values
 def parse_GDSC_rnaseq(first_degree_neighbors, cancer_type):
-    data_PATH = "/home/gil/Desktop/GCN/datasets/Data_downloaded_directly_from_source/"
+    data_PATH = PATH+"Data_from_source/GDSC/"
 
     # read GDSC rnaseq
-    GDSC_df = pd.read_csv(data_PATH + 'GDSC_Cell_line_RMA_proc_basalExp.csv')
-    # read GDSC cell line-target-drugs data data
+    GDSC_df = pd.read_csv(data_PATH + 'Cell_line_RMA_proc_basalExp.csv', sep='\t')
+    # read GDSC cell line-target-drugs data
     GDSC_cell_target_drug  = pd.read_csv(data_PATH + 'GDSC2_fitted_dose_response_25Feb20.csv').loc[:,
                            ['COSMIC_ID', 'CELL_LINE_NAME', 'TCGA_DESC', 'DRUG_NAME', 'PUTATIVE_TARGET','LN_IC50', 'AUC']].drop_duplicates()
     COSMIC_to_Cell_line_name = GDSC_cell_target_drug.loc[:, ['COSMIC_ID', 'CELL_LINE_NAME']].drop_duplicates()
@@ -97,59 +105,150 @@ def parse_GDSC_rnaseq(first_degree_neighbors, cancer_type):
     return GDSC_df
 
 
+# Load GDSC rnaseq data and parse:
+# a. keep only 1st degree
+# b. change column names to cell line names
+# c. get rid of unnecessary columns
+# d. transpose
+# e. compute zscore(log(rsem values))
+# f. add first degree missing columns as 0 values
+def parse_ENLIGHT_rnaseq(first_degree_neighbors, cancer_type, sorafenib_data = 0):
+    data_PATH = PATH+"Data_from_source/ENLIGHT/"
+
+    # read rnaseq
+    if cancer_type == "BRCA" or sorafenib_data == 2:
+        ENLIGHT_df = pd.read_csv(data_PATH + 'sorafenib_2.csv', sep='\t')
+    if cancer_type == "LIHC" or sorafenib_data == 1:
+        ENLIGHT_df = pd.read_csv(data_PATH + 'sorafenib.csv', sep='\t')
+
+
+
+    # read GDSC cell line-target-drugs data
+    ENLIGHT_cell_target_drug  = pd.read_csv(data_PATH + 'Enlight_drug_response_classification.csv')
+
+
+    # get hugo values in intersect of ENLIGHT and 1st deg neighbors
+    in_first_deg_and_GDSC = [x for x in ENLIGHT_df['GENE_SYMBOLS'] if x in first_degree_neighbors]
+    # use hugo as index so I can call loc for 1st deg neighbors.
+    ENLIGHT_df.index = ENLIGHT_df['GENE_SYMBOLS']
+    # keep in ENLIGHT only intersect genes, by the order of the intersect vector
+    ENLIGHT_df = ENLIGHT_df.loc[in_first_deg_and_GDSC]
+
+
+### todo: Got to this point with this function for ENLIGHT 16.12.2024
+
+
+    # Transpose, get rid of first 2 columns
+    ENLIGHT_df = ENLIGHT_df.iloc[:,2:].transpose()
+    ENLIGHT_df.index = [int(str(ind).split(".")[1]) for ind in ENLIGHT_df.index]
+    ENLIGHT_df = ENLIGHT_df.drop_duplicates()
+    ENLIGHT_df.index = ENLIGHT_df.
+
+
+    # Parse GDSC into z scores similar to TCGA - zscore(log(rna_seq values))
+    ENLIGHT_df = np.log(ENLIGHT_df + (1e-10))
+    for col in ENLIGHT_df.columns:
+        ENLIGHT_df[col] = scipy.stats.zscore(ENLIGHT_df[col])
+
+
+    # keep only cell line from relevant tissue before normalization
+    if cancer_type != "pan_cancer":
+        GDSC_cell_line_to_tissue = GDSC_cell_target_drug.loc[:, ["CELL_LINE_NAME", "TCGA_DESC"]].drop_duplicates()
+        if cancer_type in ["COAD", "READ"]:
+            tissue_logic = GDSC_cell_line_to_tissue['TCGA_DESC'] == "COREAD"
+            GDSC_df = GDSC_df.loc[[cosm for cosm in GDSC_df.index if cosm in GDSC_cell_line_to_tissue.loc[tissue_logic,"CELL_LINE_NAME"].values]]
+        else:
+            tissue_logic = GDSC_cell_line_to_tissue['TCGA_DESC'] == cancer_type
+            GDSC_df = GDSC_df.loc[[cosm for cosm in GDSC_df.index if cosm in GDSC_cell_line_to_tissue.loc[tissue_logic,"CELL_LINE_NAME"].values]]
+
+
+    # add and reorder GDSC columns to fit TCGA
+    add_columns = [col for col in gene_model.X.columns if col not in GDSC_df.columns]
+    # for cases where model.X.columns have duplicate values (it happens for some reason with EGFR in GBM)
+    [GDSC_df.insert(0, col, 0) for col in add_columns]
+
+    GDSC_df = GDSC_df.reindex(columns=first_degree_neighbors)
+
+    # fix (hopefully) a specific bug in EGFR
+    # if gene == "EGFR":
+    #     GDSC_df.insert(120, 'AK4', 0, allow_duplicates=True)
+    return GDSC_df
+
+
 ### MAIN ###
 if __name__ == '__main__':
     # paths
-    PATH_GCN = "/home/gil/Desktop/GCN/"
-    data_PATH = "/home/gil/Desktop/GCN/datasets/Data_downloaded_directly_from_source/"
-    results_PATH = "/home/gil/Desktop/GCN/GCN_analysis/10.CCLE/GDSC_models_3.0/"
+    results_PATH = PATH+"Data_generated/STAMP_predictions/"
 
-    # define model/gene/cancer_type/drug to investigate
-    data_type = "Pathways"
-    model_type = "GCN"
+
     delay_seconds = 5
-    genes_cancer_types = {"PI3K": ["LUAD", "LUSC", "BRCA"]}#"TP53":["BRCA", "LUAD", "LGG"]}#"EGFR":["GBM"], "ERBB2":["BRCA"], "KRAS":["LUAD"], "BRAF":["SKCM", "THCA"]} #, "COAD", "READ", "PAAD"], "RTK RAS":["BRCA", "UCEC", "THCA", "LGG", "LUSC", "HNSC", "LUAD", "SKCM", "BLCA", "COAD", "STAD", "CESC", "SARC", "ESCA", "PAAD", "GBM", "READ", "OV", "LAML", "TGCT", "PCPG", "MESO", "CHOL"]}
 
-    for gene in genes_cancer_types.keys():
-        for cancer_type in genes_cancer_types[gene]:
+    data_to_predict = ["ENLIGHT"] # ["GDSC", "ENLIGHT", "CCLE"]
 
-            # get model
-            gene_model = load_model(data_type, model_type, cancer_type, gene)
-            # 1st degree neighbors
-            gene_first_degree_neighbors = gene_model.X.columns.drop_duplicates()
+    genes_cancer_types = {
+    "RTK RAS": ["pan_cancer", "LIHC", "BRCA", "LUAD", "LUSC", "HNSC"]
+    # "BRAF": ["pan_cancer", "LUAD"],
+    # "EGFR": ["pan_cancer", "LIHC", "BRCA"],
+    # "KRAS": ["pan_cancer", "LIHC", "BRCA"],
+    # "MET": ["pan_cancer", "LIHC", "BRCA"],
+    # "NF1": ["pan_cancer", "LIHC", "BRCA"],
+    # "RASA1": ["pan_cancer", "LIHC", "BRCA"],
+    # "ERRFI1": ["pan_cancer", "LIHC", "BRCA"],
+    # "FGFR1": ["pan_cancer", "BRCA", "LUSC", "HNSC"]
+    }
 
-            # parsre GDSC for model predictions:
-            GDSC_first_deg_z_scored = parse_GDSC_rnaseq(gene_first_degree_neighbors, cancer_type)
+    for data in data_to_predict:
+        for gene in genes_cancer_types.keys():
+            for cancer_type in genes_cancer_types[gene]:
+                for model_type in ["GCN", "ELR", "RF"]:
+                    # get model
+                    gene_model = load_model(model_type, cancer_type, gene)
+                    #ToDo: check if this (all following lines) works for ELR, RF
 
-            # get predictions for GDSC
-            gene_model_predict = gene_model.predict(GDSC_first_deg_z_scored)
-            GDSC_predict_bool = np.argmax(gene_model_predict, axis=1)
-            # calculate linear score, avoid infinity values
-            GDSC_predict_linear = scipy.special.logit(gene_model_predict[:, 1])
-            max_value = max(GDSC_predict_linear[GDSC_predict_linear != math.inf])
-            min_value = min(GDSC_predict_linear[GDSC_predict_linear != -math.inf])
-            GDSC_predict_linear[GDSC_predict_linear == math.inf] = max_value + 1
-            GDSC_predict_linear[GDSC_predict_linear == -math.inf] = min_value - 1
+                    # 1st degree neighbors
+                    gene_first_degree_neighbors = gene_model.X.columns.drop_duplicates()
 
-            # create GDSC dataframe with GCN preds
-            GDSC_predictions = pd.DataFrame(GDSC_first_deg_z_scored.index)
-            GDSC_predictions.index = GDSC_predictions["CELL_LINE_NAME"]
-            GDSC_predictions = GDSC_predictions.rename(columns={0: "GDSC Cell Line Name"})
-            GDSC_predictions["GCN_pred"] = GDSC_predict_bool.numpy()
-            GDSC_predictions["GCN_linear"] = GDSC_predict_linear.numpy()
-            GDSC_predictions = GDSC_predictions.iloc[:,1:]
+                    if data == "GDSC":
+                        # parsre GDSC for model predictions:
+                        data_first_deg_z_scored = parse_GDSC_rnaseq(gene_first_degree_neighbors, cancer_type)
+                    if data == "ENLIGHT":
+                        if cancer_type == "pan_cancer":
+                            # Change sorafenib_data to 2 if you want Breast Data
+                            data_first_deg_z_scored = parse_ENLIGHT_rnaseq(gene_first_degree_neighbors, cancer_type, sorafenib_data = 1)
+                        else:
+                            data_first_deg_z_scored = parse_ENLIGHT_rnaseq(gene_first_degree_neighbors, cancer_type,
+                                                                           sorafenib_data=0)
 
-            GDSC_predictions.to_csv(results_PATH+"GDSC_cell_lines_zscored_pan_GDSC_"+gene+"_"+cancer_type+"_GCN_predictions.csv")
+                    # get predictions for GDSC
+                    gene_model_predict = gene_model.predict(data_first_deg_z_scored)
+                    data_predict_bool = np.argmax(gene_model_predict, axis=1)
+                    # calculate linear score, avoid infinity values
+                    data_predict_linear = scipy.special.logit(gene_model_predict[:, 1])
+                    max_value = max(data_predict_linear[data_predict_linear != math.inf])
+                    min_value = min(data_predict_linear[data_predict_linear != -math.inf])
+                    data_predict_linear[data_predict_linear == math.inf] = max_value + 1
+                    data_predict_linear[data_predict_linear == -math.inf] = min_value - 1
 
-            del gene_model
-            torch.cuda.empty_cache()
+                    # create dataframe with GCN preds for selected data
+                    data_predictions = pd.DataFrame(data_first_deg_z_scored.index)
+                    data_predictions.index = data_predictions["Sample_name"]
+                    data_predictions = data_predictions.rename(columns={0: data +"_Sample_name"})
+                    data_predictions["GCN_pred"] = data_predict_bool.numpy()
+                    data_predictions["GCN_linear"] = data_predict_linear.numpy()
+                    data_predictions = data_predictions.iloc[:,1:]
 
-            # # load GDSC cosmic to cell line name data (drugs effect data)
-            # GDSC_cosmic_to_cell_line = pd.read_csv(data_PATH + 'GDSC2_fitted_dose_response_25Feb20.csv').loc[:,
-            #                            ['COSMIC_ID', 'CELL_LINE_NAME', 'TCGA_DESC', 'DRUG_NAME', 'PUTATIVE_TARGET','LN_IC50', 'AUC']].drop_duplicates()
-            #
-            # Dabrafenib_IC_50 = GDSC_cosmic_to_cell_line.loc[GDSC_cosmic_to_cell_line['DRUG_NAME'] == "PLX-4720", 'LN_IC50']
-            # Dabrafenib_IC_50 = Dabrafenib_IC_50.loc[[val for val in Dabrafenib_IC_50.index if val in GDSC_predictions.index]]
-            # GDSC_IC50 = GDSC_predictions.loc[Dabrafenib_IC_50.index]
-            # GDSC_IC50['Dabrafenib_IC50'] = Dabrafenib_IC_50.loc[GDSC_IC50.index]
-            # plt.scatter(GDSC_IC50['GCN_linear'], GDSC_IC50['Dabrafenib_IC50'])
+                    data_predictions.to_csv(results_PATH+data+"/data_zscored_pan_"+data+"_"+gene+"_"+cancer_type+"_"+model_type+"_predictions.csv")
+
+                    del gene_model
+                    if model_type == "GCN":
+                        torch.cuda.empty_cache()
+
+                    # # load GDSC cosmic to cell line name data (drugs effect data)
+                    # GDSC_cosmic_to_cell_line = pd.read_csv(data_PATH + 'GDSC2_fitted_dose_response_25Feb20.csv').loc[:,
+                    #                            ['COSMIC_ID', 'CELL_LINE_NAME', 'TCGA_DESC', 'DRUG_NAME', 'PUTATIVE_TARGET','LN_IC50', 'AUC']].drop_duplicates()
+                    #
+                    # Dabrafenib_IC_50 = GDSC_cosmic_to_cell_line.loc[GDSC_cosmic_to_cell_line['DRUG_NAME'] == "PLX-4720", 'LN_IC50']
+                    # Dabrafenib_IC_50 = Dabrafenib_IC_50.loc[[val for val in Dabrafenib_IC_50.index if val in GDSC_predictions.index]]
+                    # GDSC_IC50 = GDSC_predictions.loc[Dabrafenib_IC_50.index]
+                    # GDSC_IC50['Dabrafenib_IC50'] = Dabrafenib_IC_50.loc[GDSC_IC50.index]
+                    # plt.scatter(GDSC_IC50['GCN_linear'], GDSC_IC50['Dabrafenib_IC50'])
